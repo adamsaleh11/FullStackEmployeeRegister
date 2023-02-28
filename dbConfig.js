e4476d98edace0be122e56ff33494bbbe248700d
adamsaleh11 <58434919+adamsaleh11@users.noreply.github.com>
2023-02-28 13:36:13 -0500

    Upload database files

---
+++ dbConfig.js
@@ -0,0 +1,14 @@
+const config = {
+	user: 'saleha',
+	password: 'adam',
+	server:'DESKTOP-GEBQCUS',
+	database:'SQL Tutorial1',
+	port: 1433,
+	options: {
+		trustServerCertificate: true,
+		trustedConnection: true,
+		enableArithAbort: true,
+		instancename:'SQLEXPRESS'
+	}
+}
+module.exports = config;
---
+++ dbOperations.js
@@ -0,0 +1,30 @@
+const config  = require('./dbConfig');
+const sql = require('mssql');
+
+const getEmployees = async(firstname) => {
+	try {
+		let pool = await sql.connect(config)
+		let employees = await pool.request().query(`SELECT * from EmployeeDemographics WHERE Firstname = '${firstname}' `)
+		console.log(employees);
+		return employees;
+	} catch(error) {
+		console.log(error);
+	}
+}
+
+const createEmployees = async(Employee) => {
+	try {
+		let pool = await sql.connect(config)
+		let employees = await pool.request().query(`INSERT INTO EmployeeDemographics VALUES 
+		(${Employee.EmployeeID},'${Employee.Firstname}', '${Employee.Lastname}', ${Employee.Age}, '${Employee.Gender}')`)
+		console.log(employees);
+		return employees;
+	} catch(error) {
+		console.log(error);
+	}
+}
+
+module.exports = {
+	createEmployees,
+	getEmployees
+}
